c0727fa6af8d5a954739107ab08a7e3cb5135003
Luca Spalierno <spalierno@gmail.com>
2020-10-02 23:00:36 +0200

    Add widget test and remove default test (broken at this point).

---
+++ test/create_new_task_page_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task_planner_app/screens/create_new_task_page.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Title and date pickers are available',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: CreateNewTaskPage(),
+          ),
+        );
+
+        final titleFinder = find.text('Title');
+        final dateFinder = find.text('Date');
+
+        expect(titleFinder, findsOneWidget);
+        expect(dateFinder, findsOneWidget);
+      });
+
+  test('Title and date pickers are available', () {});
+}
