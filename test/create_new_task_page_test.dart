c9d9b33714b3596d1922b17978f7bedef2a7be37
Luca Spalierno <spalierno@gmail.com>
2020-10-03 17:38:13 +0200

    Reformat file.

--- test/create_new_task_page_test.dart
+++ test/create_new_task_page_test.dart
@@ -4,17 +4,17 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Title and date pickers are available',
-          (WidgetTester tester) async {
-        await tester.pumpWidget(
-          MaterialApp(
-            home: CreateNewTaskPage(),
-          ),
-        );
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: CreateNewTaskPage(),
+      ),
+    );
 
-        final titleFinder = find.text('Title');
-        final dateFinder = find.text('Date');
+    final titleFinder = find.text('Title');
+    final dateFinder = find.text('Date');
 
-        expect(titleFinder, findsOneWidget);
-        expect(dateFinder, findsOneWidget);
-      });
+    expect(titleFinder, findsOneWidget);
+    expect(dateFinder, findsOneWidget);
+  });
 }
